f33e6b9ade6d0a4767967cd576d855fe9aa48da4
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2026-03-05 12:52:05 -0300

    05/03

---
+++ aula-05-03-2026/script.js
@@ -0,0 +1,23 @@
+console,log("Teste em Javascript");
+
+//variaveis
+var animal = "cavalo";
+console.log(animal);
+
+let nomeCompleto = 'Fulano da Silva'
+console.log(nomeCompleto)
+
+var valor1;
+const valor0 = 10;
+console.log(valor0);
+
+valor1 = 12; 
+valor1 = 15; // var, permite reatribuição
+//valor0 = 14; --> não permite reatribuição
+
+// verificação de tipos
+console.log(typeof valor1);
+
+// algoritmo: entrada + processamento + saida
+var nome = window.prompt ("Nome");
+console.log("Seja bem-vindo" + nome);
